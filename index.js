f18df3398db680eb54736b3fa7364acf60d06b16
Daryl <darylwong@primustech.com.sg>
2020-09-01 22:52:07 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const upload = require("express-fileupload");
+
+const app = express();
+
+app.use(upload());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  if (req.files) {
+    console.log(req.files);
+
+    var file = req.files.file;
+    var filename = file.name;
+    console.log(filename);
+
+    file.mv("./uploads/" + filename, function (err) {
+      if (err) {
+        console.log(err);
+        res.send(err);
+      } else {
+        res.send("File uploaded");
+      }
+    });
+  }
+});
+
+app.listen(5000);
